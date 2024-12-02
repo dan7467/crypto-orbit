52073b28b32d37aff31944e4e6fdad1c6eb31f59
dan7467 <dan7467@gmail.com>
2024-12-02 23:26:51 +0200

    controller and service extended

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,19 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Post } from '@nestjs/common';
 import { AppService } from './app.service';
 
 @Controller()
 export class AppController {
+  
   constructor(private readonly appService: AppService) {}
 
   @Get()
-  getHello(): string {
-    return this.appService.getHello();
+  getTest(): string {
+    return this.appService.getTest();
   }
+
+  @Post()
+  postTest(): string {
+    return this.appService.postTest();
+  }
+
 }
--- src/app.service.ts
+++ src/app.service.ts
@@ -2,7 +2,12 @@ import { Injectable } from '@nestjs/common';
 
 @Injectable()
 export class AppService {
-  getHello(): string {
-    return 'Hello World!';
+
+  getTest(): string {
+    return 'sent GET req';
+  }
+
+  postTest(): string {
+    return 'sent POST req';
   }
 }
